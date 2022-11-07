646086c281313e224c725bf8eb52f6b547e9fdb8
Andrii Dovhaliuk <andy87@live.ru>
2022-11-07 19:55:50 +0400

    Change to get values from the form instead of inputs

--- javascript.js
+++ javascript.js
@@ -2,111 +2,103 @@ let myLibrary = [];
 
 class Book {
   constructor(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
     this.isRead = read || false;
   }
   info() {
-    let readString = (this.isRead) ? 'read already' : 'not read yet';
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${readString}.`
+    let readString = this.isRead ? "read already" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readString}.`;
   }
 }
 
-const book1 = new Book('1984', 'George Orwell', '328', true);
-const book2 = new Book('Where the Crawdads Sing', 'Delia Owens', '368', false);
+const book1 = new Book("1984", "George Orwell", "328", true);
+const book2 = new Book("Where the Crawdads Sing", "Delia Owens", "368", false);
 myLibrary.push(book1, book2);
 
-const booksContainer = document.querySelector('.books-container');
-const modal = document.querySelector('.modal');
-const newBookButton = document.getElementById('new-book');
-const closeButton = document.querySelector('.close');
-const submitBookButton = document.getElementById('submit');
+const booksContainer = document.querySelector(".books-container");
+const modal = document.querySelector(".modal");
+const newBookButton = document.getElementById("new-book");
+const closeButton = document.querySelector(".close");
+const addBookForm = document.getElementById("add-book-form");
 
-newBookButton.addEventListener('click', openModal);
-closeButton.addEventListener('click', closeModal);
-submitBookButton.addEventListener('click', addBookToLibrary);
+newBookButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+addBookForm.addEventListener("submit", addBookToLibrary);
 
 function openModal() {
-  modal.style.display = 'flex';
+  modal.style.display = "flex";
 }
 
 function closeModal() {
-  clearAddBookValues();
-  modal.style.display = 'none';
+  addBookForm.reset();
+  modal.style.display = "none";
 }
 
-function addBookToLibrary() {
-  let title = document.querySelector("#add-title").value;
-  let author = document.querySelector("#add-author").value;
-  let pages = document.querySelector("#add-pages").value;
-  let read = document.querySelector("#read").checked;
+function addBookToLibrary(e) {
+  e.preventDefault();
+  let title = e.target[0].value;
+  let author = e.target[1].value;
+  let pages = e.target[2].value;
+  let read = e.target[3].checked;
 
   myLibrary.push(new Book(title, author, pages, read));
-  clearAddBookValues();
-  modal.style.display = 'none';
+  addBookForm.reset();
+  modal.style.display = "none";
   showBooks(myLibrary);
-}
-
-function clearAddBookValues() {
-  document.querySelector("#add-title").value = '';
-  document.querySelector("#add-author").value = '';
-  document.querySelector("#add-pages").value = '';
-  document.querySelector("#read").checked = false;
 }
 
 function showBooks(library) {
   let id = 0;
-  library.forEach(library_book => {
+  library.forEach((library_book) => {
     if (document.querySelector(`#book-${id}`) === null) {
       let bookCard = createBookCard(library_book, id);
       booksContainer.appendChild(bookCard);
     }
     id++;
-  })
+  });
 }
 
-
 function createBookCard(book, id) {
-  let bookCard = document.createElement('div');
-  bookCard.classList.add('book-card');
-  bookCard.setAttribute('id', `book-${id}`)
+  let bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+  bookCard.setAttribute("id", `book-${id}`);
 
-  let title = document.createElement('div');
-  title.classList.add('title');
+  let title = document.createElement("div");
+  title.classList.add("title");
   title.textContent = `Title: ${book.title}`;
 
-  let author = document.createElement('div');
-  author.classList.add('author');
+  let author = document.createElement("div");
+  author.classList.add("author");
   author.textContent = `Author: ${book.author}`;
 
-  let pages = document.createElement('div');
-  pages.classList.add('pages');
+  let pages = document.createElement("div");
+  pages.classList.add("pages");
   pages.textContent = `Pages: ${book.pages}`;
 
-  let isRead = document.createElement('div');
-  isRead.classList.add('isRead');
-  isRead.setAttribute('id', `checkbox-container-${id}`)
-  let label = document.createElement('label');
-  label.setAttribute('for', 'isRead');
-  label.textContent = 'Mark as read:'
-  let checkbox = document.createElement('input');
-  checkbox.setAttribute('type', 'checkbox')
-  checkbox.setAttribute('id', 'isRead');
+  let isRead = document.createElement("div");
+  isRead.classList.add("isRead");
+  isRead.setAttribute("id", `checkbox-container-${id}`);
+  let label = document.createElement("label");
+  label.setAttribute("for", "isRead");
+  label.textContent = "Mark as read:";
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("id", "isRead");
   if (book.isRead) {
     checkbox.checked = true;
-    bookCard.classList.add('read-already');
+    bookCard.classList.add("read-already");
   }
-  checkbox.setAttribute('onchange', 'updateIsRead(this.parentElement.id)');
+  checkbox.setAttribute("onchange", "updateIsRead(this.parentElement.id)");
   isRead.appendChild(label);
   isRead.appendChild(checkbox);
 
-  let deleteButton = document.createElement('button');
-  deleteButton.textContent = 'Delete';
-  deleteButton.setAttribute('id', `delete-book-${id}`);
-  deleteButton.classList.add('delete-book');
-  deleteButton.setAttribute('onclick', 'deleteBook(this.parentElement.id)');
-
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Delete";
+  deleteButton.setAttribute("id", `delete-book-${id}`);
+  deleteButton.classList.add("delete-book");
+  deleteButton.setAttribute("onclick", "deleteBook(this.parentElement.id)");
 
   bookCard.appendChild(title);
   bookCard.appendChild(author);
@@ -114,7 +106,7 @@ function createBookCard(book, id) {
   bookCard.appendChild(isRead);
   bookCard.appendChild(deleteButton);
 
-  return bookCard
+  return bookCard;
 }
 
 function deleteBook(id) {
@@ -126,14 +118,10 @@ function deleteBook(id) {
 }
 
 function updateIsRead(id) {
-  let bookId = id.split('-')[2];
+  let bookId = id.split("-")[2];
   let bookToBeUpdated = document.getElementById(`book-${bookId}`);
-  bookToBeUpdated.classList.toggle('read-already');
+  bookToBeUpdated.classList.toggle("read-already");
   myLibrary[bookId].isRead = !myLibrary[bookId].isRead;
-
 }
 
 showBooks(myLibrary);
-
-
-
